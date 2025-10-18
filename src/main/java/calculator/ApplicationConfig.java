ab3382e2be3502e4cd84493a29202cea82bc7e7e
MASKUN2 <maskunflower2@naver.com>
2025-10-18 19:24:19 +0900

    feat(string sum calculator): 문자열덧셈계산기 초기화 로직 추가

---
+++ src/main/java/calculator/ApplicationConfig.java
@@ -0,0 +1,24 @@
+package calculator;
+
+import calculator.adaptor.IntergerResultPrinter;
+import calculator.adaptor.RunInstructor;
+import calculator.adaptor.StringInputReader;
+import calculator.hexagon.application.StringSumCalculatorImpl;
+import calculator.hexagon.domain.*;
+import calculator.hexagon.provided.StringSumCalculator;
+
+public class ApplicationConfig {
+
+    public StringSumCalculator stringSumCalculator() {
+        return new StringSumCalculatorImpl(
+                new RunInstructor(),
+                new StringInputReader(),
+                new CustomDelimiterDeclarer(),
+                new DelimiterValidator(),
+                new StringSplitter(),
+                new IntegerParser(),
+                new IntegerCalculator(),
+                new IntergerResultPrinter()
+        );
+    }
+}
---
+++ src/test/java/calculator/ApplicationConfigTest.java
@@ -0,0 +1,18 @@
+package calculator;
+
+import calculator.hexagon.provided.StringSumCalculator;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ApplicationConfigTest {
+
+    @Test
+    void 문자열_덧셈_계산기가_초기화된다() {
+        ApplicationConfig config = new ApplicationConfig();
+
+        StringSumCalculator calculator = config.stringSumCalculator();
+
+        assertThat(calculator).isNotNull();
+    }
+}
