954c5e77ad9aabc303d98668b04d9920ae682202
MASKUN2 <maskunflower2@naver.com>
2025-10-21 10:52:35 +0900

    test(ApplicationTest): NsTest에서 생길 수 있는 오류 재현
    빈문자열을 가지고 NsTest.Run()을 하게 되면 Console.readLine()에서 NoSuchElementException이 발생

--- src/test/java/calculator/ApplicationTest.java
+++ src/test/java/calculator/ApplicationTest.java
@@ -1,27 +1,29 @@
 package calculator;
 
-import camp.nextstep.edu.missionutils.test.NsTest;
-import org.junit.jupiter.api.Test;
-
 import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.test.NsTest;
+import org.junit.jupiter.api.Test;
 
 class ApplicationTest extends NsTest {
     @Test
     void 커스텀_구분자_사용() {
         assertSimpleTest(() -> {
             run("//;\\n1");
-            assertThat(output()).contains("결과 : 1");
+            String read = Console.readLine();
+            assertThat(read).contains("//;\\n1"); // 통과한다.
         });
     }
-
+    
     @Test
-    void 예외_테스트() {
-        assertSimpleTest(() ->
-            assertThatThrownBy(() -> runException("-1,2,3"))
-                .isInstanceOf(IllegalArgumentException.class)
-        );
+    void 빈문자열_테스트() {
+        assertSimpleTest(() -> {
+            run("");
+            String read = Console.readLine(); //java.util.NoSuchElementException: No line found
+            assertThat(read).contains("");
+        });
     }
 
     @Override
